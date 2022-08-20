6fc78be6ce49927ea024afff2d1788178f2396d3
FITRAH IZUL FALAQ <fitrahizulfalaq@gmail.com>
2022-08-20 12:06:51 +0700

    rubah profil pengembang

--- application/views/page/pembuat.php
+++ application/views/page/pembuat.php
@@ -1,11 +1,18 @@
 <div id="appCapsule">
 	<div class="section mt-2">
 		<div class="text-center">
-			<img class="imaged img-fluid" src="https://3.bp.blogspot.com/-krZ0g_xkXYo/WlbZSxYb05I/AAAAAAAAC2s/s-eJodFOyNotBtCtC25VPOejy0n5-1L5ACLcBGAs/s1600/FITRAH.jpg">
+			<!-- <img class="imaged img-fluid" src="https://3.bp.blogspot.com/-krZ0g_xkXYo/WlbZSxYb05I/AAAAAAAAC2s/s-eJodFOyNotBtCtC25VPOejy0n5-1L5ACLcBGAs/s1600/FITRAH.jpg"> -->
 		</div>
 		<br>
 		<div class="text-justify">
-			Fitrah Izul Falaq lulusan Teknologi Pendidikan UM angkatan 2016, selama aktif kuliah, dinobatkan menjadi Mahasiswa Berprestasi 3 UM 2018, dan Ketua Komisariat PMII Sunan Kalijaga UM
+			<!-- Fitrah Izul Falaq lulusan Teknologi Pendidikan UM angkatan 2016, selama aktif kuliah, dinobatkan menjadi Mahasiswa Berprestasi 3 UM 2018, dan Ketua Komisariat PMII Sunan Kalijaga UM -->
 		</div>
+		<div class="text-center">
+			<img class="imaged img-fluid" src="<?=base_url()?>/assets/img/default/1x1.png"><br>
+			&copy; 2022			
+		</div>
+		<!-- <div class="text-justify">
+			&copy; 2022
+		</div> -->
 	</div>
 </div>
