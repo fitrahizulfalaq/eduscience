a7484a660124e6dd8ed45d7eba77870acc329c18
FITRAH IZUL FALAQ <fitrahizulfalaq@gmail.com>
2022-08-23 09:07:29 +0700

    tambah dan edit foto pemimbing

--- application/views/page/pembuat.php
+++ application/views/page/pembuat.php
@@ -5,7 +5,7 @@
 				<ul class="splide__list" id="splide02-list" style="transform: translateX(-2273px);">
 					<li class="splide__slide splide__slide--clone" style="margin-right: 16px; width: 381.5px;">
 						<div class="card">
-							<img src="<?= base_url()?>/assets/img/foto/pengembang/mardiah.jpg" class="card-img-top" alt="image">
+							<img src="<?= base_url() ?>/assets/img/foto/pengembang/mardiah.jpg" class="card-img-top" alt="image">
 							<div class="card-body">
 								<h5 class="card-title">Mardiah Afifa</h5>
 								<p class="card-text">
@@ -16,7 +16,7 @@
 					</li>
 					<li class="splide__slide splide__slide--clone" style="margin-right: 16px; width: 381.5px;">
 						<div class="card">
-							<img src="<?= base_url()?>/assets/img/foto/pengembang/rafli.jpg" class="card-img-top" alt="image">
+							<img src="<?= base_url() ?>/assets/img/foto/pengembang/rafli.jpg" class="card-img-top" alt="image">
 							<div class="card-body">
 								<h5 class="card-title">Rafli Fandu Ramadhani</h5>
 								<p class="card-text">
@@ -27,7 +27,7 @@
 					</li>
 					<li class="splide__slide splide__slide--clone" style="margin-right: 16px; width: 381.5px;">
 						<div class="card">
-							<img src="<?= base_url()?>/assets/img/foto/pengembang/kamilah.jpg" class="card-img-top" alt="image">
+							<img src="<?= base_url() ?>/assets/img/foto/pengembang/kamilah.jpg" class="card-img-top" alt="image">
 							<div class="card-body">
 								<h5 class="card-title">Kamilah Nada Maisa</h5>
 								<p class="card-text">
@@ -38,7 +38,7 @@
 					</li>
 					<li class="splide__slide splide__slide--clone" style="margin-right: 16px; width: 381.5px;">
 						<div class="card">
-							<img src="<?= base_url()?>/assets/img/foto/pengembang/chosmas.jpg" class="card-img-top" alt="image">
+							<img src="<?= base_url() ?>/assets/img/foto/pengembang/chosmas.jpg" class="card-img-top" alt="image">
 							<div class="card-body">
 								<h5 class="card-title">Chosmas Marzuki</h5>
 								<p class="card-text">
@@ -49,11 +49,22 @@
 					</li>
 					<li class="splide__slide" id="splide02-slide01" aria-hidden="true" tabindex="-1" style="margin-right: 16px; width: 381.5px;">
 						<div class="card">
-							<img src="<?= base_url()?>/assets/img/foto/pengembang/anita.jpg" class="card-img-top" alt="image">
+							<img src="<?= base_url() ?>/assets/img/foto/pengembang/anita.jpg" class="card-img-top" alt="image">
 							<div class="card-body">
 								<h5 class="card-title">Anita Septia</h5>
 								<p class="card-text">
 									Anita Septia Mahasiswa Pendidikan Masyarakat Universitas Sriwijaya Angkatan 2019 , selama saya aktif berkuliah saya mengikuti berbagai kegiatan sosial dan organisasi kampus maupun luar kampus. Saya merupakan salah satu pengembang aplikasi Edscience
+								</p>
+							</div>
+						</div>
+					</li>
+					<li class="splide__slide" id="splide02-slide01" aria-hidden="true" tabindex="-1" style="margin-right: 16px; width: 381.5px;">
+						<div class="card">
+							<img src="<?= base_url() ?>/assets/img/foto/pengembang/meilinda.jpg" class="card-img-top" alt="image">
+							<div class="card-body">
+								<h5 class="card-title">Meilinda</h5>
+								<p class="card-text">
+									Dr. Meilinda, M.Pd, Dosen Pendidikan Biologi Universitas Sriwijaya. Saat ini merupakan dosen pembimbing PKM-KC aplikasi EDScience
 								</p>
 							</div>
 						</div>
