dbc8a7889e200e3837106228fea078ae969b8f4a
FITRAH IZUL FALAQ <fitrahizulfalaq@gmail.com>
2022-08-26 11:25:50 +0700

    bug peserta yang belum acc untuk login

--- application/models/User_m.php
+++ application/models/User_m.php
@@ -8,7 +8,7 @@ class User_m extends CI_Model {
 		$this->db->select('*');
 		$this->db->from('tb_user');
 		$this->db->where('email',$post['username']);
-		// $this->db->where('status','1');
+		$this->db->where('status','1');
 		$this->db->where('password',sha1($post['password']));
 		$query = $this->db->get();
 		return $query; 
